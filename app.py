d215843c1b1f4cc0609705d46849a2ffa9b52062
Jeff Butt <jbutt2@wgu.edu>
2025-04-24 13:42:42 -0400

    Adding additional code for debugging.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import random
 from game_logic import get_winner, choices
 from whitenoise import WhiteNoise
+import os
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required to use sessions
@@ -35,6 +36,14 @@ def index():
         cpu_score=session.get("cpu_score", 0),
         choices=choices
     )
+
+@app.route("/list-static")
+def list_static():
+    paths = []
+    for dirpath, dirnames, filenames in os.walk("static"):
+        for file in filenames:
+            paths.append(os.path.join(dirpath, file))
+    return "<br>".join(paths)
 
 @app.route("/reset", methods=["GET"])
 def reset():
